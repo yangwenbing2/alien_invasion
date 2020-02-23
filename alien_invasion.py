080f3e76472c295ba68347ee69fe76fc1f62338f
Innocent <670961229@qq.com>
2020-02-23 15:49:48 +0800

    Bullet1

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,12 +1,11 @@
 import sys
 
 import pygame
+from pygame.sprite import Group
 
 import game_functions as gf
 from settings import Settings
 from ship import Ship
-
-# from ultraman import Ultraman
 
 def run_game():
     # 初始化游戏并创建一个屏幕对象
@@ -18,17 +17,16 @@ def run_game():
 
     # 创建一艘飞船
     ship = Ship(ai_settings, screen)
-    # ultraman = Ultraman(screen)
+    # 创建一个用于存储子弹的编组
+    bullets = Group()
 
     # 开始游戏的主循环
     while True:
 
         # 监视键盘和鼠标事件
-        gf.check_events(ship)
+        gf.check_events(ai_settings, screen, ship, bullets)
         ship.update()
-        gf.update_screen(ai_settings, screen, ship)
-
-        # 让最近绘制的屏幕可见
-        pygame.display.flip()
+        bullets.update()
+        gf.update_screen(ai_settings, screen, ship, bullets)
 
 run_game()
