19c1af549cdb3458c3efafe58be923628cbca2ce
Innocent <670961229@qq.com>
2020-02-23 21:39:00 +0800

    Alient fleet2

--- settings.py
+++ settings.py
@@ -17,3 +17,9 @@ class Settings():
         self.bullet_height = 15
         self.bullet_color = 60, 60, 60   #这是什么用法？？？
         self.bullets_allowed = 10
+
+        # 外星人的设置
+        self.alien_speed_factor = 10
+        self.fleet_drop_speed = 10
+        # fleet_direction为1表示方向向右移，为-1表示向左移
+        self.fleet_direction = 1
