f91d0dfceccf224da47d6e45fc3eca472afb1bbc
Innocent <670961229@qq.com>
2020-02-24 20:32:34 +0800

    14.3_Modify speed

--- settings.py
+++ settings.py
@@ -2,25 +2,40 @@ class Settings():
     """存储外信任入侵的所有类"""
 
     def __init__(self):
-        """初始化游戏的设置"""
+        """初始化游戏的静态设置"""
         # 屏幕设置
         self.screen_width = 1200
         self.screen_height = 800
-        self.bg_color = (0, 230, 230)
+        self.bg_color = (230, 230, 230)
 
         # 飞船的设置
-        self.ship_speed_factor = 50
         self.ship_limit = 2
 
         # 子弹的设置
-        self.bullet_speed_factor = 30
         self.bullet_width = 500
         self.bullet_height = 15
         self.bullet_color = 60, 60, 60   #这是什么用法？？？
         self.bullets_allowed = 10
 
         # 外星人的设置
-        self.alien_speed_factor = 10
         self.fleet_drop_speed = 100
-        # fleet_direction为1表示方向向右移，为-1表示向左移
+
+        # 以什么样的速度加快游戏节奏
+        self.speedup_scale = 1.1
+
+        self.initialize_dynamic_settings()
+
+    def initialize_dynamic_settings(self):
+        """初始化随游戏进行而变化的设置"""
+        self.ship_speed_factor = 50
+        self.bullet_speed_factor = 30
+        self.alien_speed_factor = 10
+
+        # fleet_direction为1表示向右；为-1表示向左
         self.fleet_direction = 1
+
+    def increase_speed(self):
+        """提高速度设置"""
+        self.ship_speed_factor *= self.speedup_scale
+        self.bullet_speed_factor *= self.speedup_scale
+        self.alien_speed_factor *= self.speedup_scale
